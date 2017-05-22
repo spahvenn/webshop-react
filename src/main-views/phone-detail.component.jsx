151fdab9fa6671ce08155f8b19767fa716a774a3
Sami Ahvenniemi <spahvenn@gmail.com>
2017-05-22 14:14:31 +0300

    Improvements to phone-detail view specs visuals

--- src/main-views/phone-detail.component.jsx
+++ src/main-views/phone-detail.component.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import Axios from 'axios';
 import ShoppingCartAddButtonContainer from '../components/shopping-cart-add-button/shopping-cart-add-button-container';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import _ from 'underscore';
 
 class PhoneDetail extends Component {
@@ -16,12 +16,12 @@ class PhoneDetail extends Component {
   componentWillMount() {
     var self = this;
     this.phoneId = this.props.params.phoneId;
-    Axios.get(process.env.PUBLIC_URL+'/phones-data/' + this.phoneId + '.json')
-    .then(function(result) {
-      self.setState({
-        phone: result.data,
-      });
-    })
+    Axios.get(process.env.PUBLIC_URL + '/phones-data/' + this.phoneId + '.json')
+      .then(function (result) {
+        self.setState({
+          phone: result.data,
+        });
+      })
   }
 
   render() {
@@ -45,7 +45,7 @@ class PhoneDetail extends Component {
         <div className="row">
           <div className="col-md-6">
             <div className="phone-images">
-              <img src={process.env.PUBLIC_URL+'/'+this.state.phone.images[0]} alt=""/>
+              <img src={process.env.PUBLIC_URL + '/' + this.state.phone.images[0]} alt=""/>
             </div>
           </div>
           <div className="col-md-6">
@@ -63,108 +63,106 @@ class PhoneDetail extends Component {
 
         <div className="row">
           <div className="col-md-12">
-            <h2>Specifications</h2>
-            <ul className="specs">
-              <li>
-                <span>Availability and Networks</span>
-                <dl>
-                  <dt>Availability</dt>
-                  <dd></dd>
-                </dl>
-              </li>
-              <li>
-                <span>Battery</span>
-                <dl>
-                  <dt>Type</dt>
-                  <dd></dd>
-                  <dt>Talk Time</dt>
-                  <dd>{this.state.phone.battery.talkTime}</dd>
-                  <dt>Standby time (max)</dt>
-                  <dd>{this.state.phone.battery.standbyTime}</dd>
-                </dl>
-              </li>
-              <li>
-                <span>Storage and Memory</span>
-                <dl>
-                  <dt>RAM</dt>
-                  <dd>{this.state.phone.storage.ram}</dd>
-                  <dt>Internal Storage</dt>
-                  <dd>{this.state.phone.storage.flash}</dd>
-                </dl>
-              </li>
-              <li>
-                <span>Connectivity</span>
-                <dl>
-                  <dt>Network Support</dt>
-                  <dd>{this.state.phone.connectivity.cell}</dd>
-                  <dt>WiFi</dt>
-                  <dd>{this.state.phone.connectivity.wifi}</dd>
-                  <dt>Bluetooth</dt>
-                  <dd>{this.state.phone.connectivity.bluetooth}</dd>
-                  <dt>Infrared</dt>
-                  <dd>{this.state.phone.connectivity.infrared}</dd>
-                  <dt>GPS</dt>
-                  <dd>{this.state.phone.connectivity.gps}</dd>
-                </dl>
-              </li>
-              <li>
-                <span>Android</span>
-                <dl>
-                  <dt>OS Version</dt>
-                  <dd>{this.state.phone.android.os}</dd>
-                  <dt>UI</dt>
-                  <dd>{this.state.phone.android.ui}</dd>
-                </dl>
-              </li>
-              <li>
-                <span>Size and Weight</span>
-                <dl>
-                  <dt>Dimensions</dt>
-                  <dt>Weight</dt>
-                  <dd>{this.state.phone.sizeAndWeight.weight}</dd>
-                </dl>
-              </li>
-              <li>
-                <span>Display</span>
-                <dl>
-                  <dt>Screen size</dt>
-                  <dd>{this.state.phone.display.screenSize}</dd>
-                  <dt>Screen resolution</dt>
-                  <dd>{this.state.phone.display.screenResolution}</dd>
-                  <dt>Touch screen</dt>
-                  <dd>{this.state.phone.display.touchScreen }</dd>
-                </dl>
-              </li>
-              <li>
-                <span>Hardware</span>
-                <dl>
-                  <dt>CPU</dt>
-                  <dd>{this.state.phone.hardware.cpu}</dd>
-                  <dt>USB</dt>
-                  <dd>{this.state.phone.hardware.usb}</dd>
-                  <dt>Audio / headphone jack</dt>
-                  <dd>{this.state.phone.hardware.audioJack}</dd>
-                  <dt>FM Radio</dt>
-                  <dd>{this.state.phone.hardware.fmRadio }</dd>
-                  <dt>Accelerometer</dt>
-                  <dd>{this.state.phone.hardware.accelerometer }</dd>
-                </dl>
-              </li>
-              <li>
-                <span>Camera</span>
-                <dl>
-                  <dt>Primary</dt>
-                  <dd>{this.state.phone.camera.primary}</dd>
-                  <dt>Features</dt>
-                  <dd>{this.state.phone.camera.features.join(', ')}</dd>
-                </dl>
-              </li>
-              <li>
-                <span>Additional Features</span>
-                <dd>{this.state.phone.additionalFeatures}</dd>
-              </li>
-            </ul>
-
+            <h2 className="specs-title">Specifications</h2>
+          </div>
+        </div>
+        <div className="specs">
+          <div className="row">
+            <div className="col-lg-2 col-md-3 col-sm-3 col-xs-6 col-lg-offset-2">
+              <span>Battery</span>
+              <dl>
+                <dt>Type</dt>
+                <dd>{this.state.phone.battery.type}</dd>
+                <dt>Talk Time</dt>
+                <dd>{this.state.phone.battery.talkTime}</dd>
+                <dt>Standby time (max)</dt>
+                <dd>{this.state.phone.battery.standbyTime}</dd>
+              </dl>
+            </div>
+            <div className="col-lg-2 col-md-3 col-sm-3 col-xs-6">
+              <span>Storage and Memory</span>
+              <dl>
+                <dt>RAM</dt>
+                <dd>{this.state.phone.storage.ram}</dd>
+                <dt>Internal Storage</dt>
+                <dd>{this.state.phone.storage.flash}</dd>
+              </dl>
+            </div>
+            <div className="col-lg-2 col-md-3 col-sm-3 col-xs-6">
+              <span>Connectivity</span>
+              <dl>
+                <dt>Network Support</dt>
+                <dd>{this.state.phone.connectivity.cell}</dd>
+                <dt>WiFi</dt>
+                <dd>{this.state.phone.connectivity.wifi}</dd>
+                <dt>Bluetooth</dt>
+                <dd>{this.state.phone.connectivity.bluetooth}</dd>
+                <dt>Infrared</dt>
+                <dd>{this.state.phone.connectivity.infrared}</dd>
+                <dt>GPS</dt>
+                <dd>{this.state.phone.connectivity.gps}</dd>
+              </dl>
+            </div>
+            <div className="col-lg-2 col-md-3 col-sm-3 col-xs-6">
+              <span>Android</span>
+              <dl>
+                <dt>OS Version</dt>
+                <dd>{this.state.phone.android.os}</dd>
+                <dt>UI</dt>
+                <dd>{this.state.phone.android.ui}</dd>
+              </dl>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-lg-2 col-md-3 col-sm-3 col-xs-6 col-lg-offset-2">
+              <span>Size and Weight</span>
+              <dl>
+                <dt>Dimensions</dt>
+                <dt>Weight</dt>
+                <dd>{this.state.phone.sizeAndWeight.weight}</dd>
+              </dl>
+            </div>
+            <div className="col-lg-2 col-md-3 col-sm-3 col-xs-6">
+              <span>Display</span>
+              <dl>
+                <dt>Screen size</dt>
+                <dd>{this.state.phone.display.screenSize}</dd>
+                <dt>Screen resolution</dt>
+                <dd>{this.state.phone.display.screenResolution}</dd>
+                <dt>Touch screen</dt>
+                <dd>{this.state.phone.display.touchScreen }</dd>
+              </dl>
+            </div>
+            <div className="col-lg-2 col-md-3 col-sm-3 col-xs-6">
+              <span>Hardware</span>
+              <dl>
+                <dt>CPU</dt>
+                <dd>{this.state.phone.hardware.cpu}</dd>
+                <dt>USB</dt>
+                <dd>{this.state.phone.hardware.usb}</dd>
+                <dt>Audio / headphone jack</dt>
+                <dd>{this.state.phone.hardware.audioJack}</dd>
+                <dt>FM Radio</dt>
+                <dd>{this.state.phone.hardware.fmRadio }</dd>
+                <dt>Accelerometer</dt>
+                <dd>{this.state.phone.hardware.accelerometer }</dd>
+              </dl>
+            </div>
+            <div className="col-lg-2 col-md-3 col-sm-3 col-xs-6">
+              <span>Camera</span>
+              <dl>
+                <dt>Primary</dt>
+                <dd>{this.state.phone.camera.primary}</dd>
+                <dt>Features</dt>
+                <dd>{this.state.phone.camera.features.join(', ')}</dd>
+              </dl>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-lg-2 col-md-3 col-sm-3 col-xs-6 col-lg-offset-2">
+              <span>Additional Features</span>
+              <dd>{this.state.phone.additionalFeatures}</dd>
+            </div>
           </div>
         </div>
       </div>
@@ -172,7 +170,7 @@ class PhoneDetail extends Component {
   }
 }
 
-const mapStateToProps = function(store) {
+const mapStateToProps = function (store) {
   return {
     shoppingCart: store.shoppingCartState
   };
